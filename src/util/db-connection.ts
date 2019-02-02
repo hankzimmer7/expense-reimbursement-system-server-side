b63adf1fbd4e3e8d22ea8403ffc7bfb13fa5f3cf
hankzimmer7 <hankzimmer7@gmail.com>
2019-02-02 18:22:08 -0500

    adds some db connection info

---
+++ src/util/db-connection.ts
@@ -0,0 +1,10 @@
+import { Pool } from 'pg';
+
+export const connectionPool = new Pool({
+  user: process.env.aws_db_user,
+  host: process.env.aws_db_host,
+  database: process.env.aws_db_name,
+  password: process.env.aws_db_password,
+  port: 5432,
+  max: 5
+});
