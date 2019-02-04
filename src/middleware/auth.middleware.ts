fae2bf21f33f759085a83a93fea8a43820576492
hankzimmer7 <hankzimmer7@gmail.com>
2019-02-04 13:17:04 -0500

    removes authorization layer to troubleshoot db connection

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,9 +1,9 @@
 export function authMiddleware(req, res, next) {
-  const user = req.session.user;
-  if (user && (user.role === 1 || user.role === 2)) {
+  // const user = req.session.user;
+  // if (user && (user.role === 1 || user.role === 2)) {
     next();
-  } else {
-    res.status(401);
-    res.json({message: 'The incoming token has expired'});
-  }
+  // } else {
+  //   res.status(401);
+  //   res.json({message: 'The incoming token has expired'});
+  // }
 }
